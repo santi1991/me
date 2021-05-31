3d17b695f355ca51b0c173aa77ae1d1f1faf8298
Santiago Marulanda <santiago.marulandam@gmail.com>
2021-05-31 16:22:53 -0500

    hello world message added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>This is my App</h1>
+        <h2>Hello World!</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
